5d406d4b649e39798d985ad720b80ff8330381c3
Demask90 <84012055+Demask90@users.noreply.github.com>
2021-07-19 15:22:23 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 
 var numbers = 100;
-var  y = (i + 1);
+
 
 for (var i = 0; i < numbers; ++i) {
 
+    var  y = (i + 1);
     //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-    if ((y % 3 == 0) && (y % 5 == 0))
+    //per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+    if (((y % 3) == 0) && ((y % 5) == 0))
     { 
         console.log("FizzBuzz");
     } 
-    // per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-    else if ((y % 3 == 0) && (y % 5 !== 0))
+    else if (((y % 3) == 0) && ((y % 5) !== 0))
     { 
         console.log("Fizz");
     } 
-    else if ((y % 5 == 0) && (y % 3 !== 0))
+    else if (((y % 5) == 0) && ((y % 3) !== 0))
     {
         console.log("Buzz");
     }
